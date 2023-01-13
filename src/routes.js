837dffe3b5cb81762eb46df97bbb08abb1f2f17d
Alexandre <alexandrecarlos2@gmai.com>
2023-01-12 22:12:34 -0300

    adicionando contexto de carrinho

--- src/routes.js
+++ src/routes.js
@@ -3,22 +3,25 @@ import Feira from 'pages/Feira';
 import Login from 'pages/Login';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { UsuarioProvider } from 'common/context/Usuario';
+import { CarrinhoProvider } from 'common/context/Carrinho';
 
 function Router() {
     return (
         <BrowserRouter>
             <Switch>
+            <UsuarioProvider>
                 <Route exact path='/'>
-                    <UsuarioProvider>
-                        <Login />
-                    </UsuarioProvider>
+                    <Login />
                 </Route> 
-                <Route path='/feira'>
-                    <Feira/>
-                </Route>
-                <Route path='/carrinho'>
-                    <Carrinho/>
+                <CarrinhoProvider>
+                    <Route path='/feira'>
+                        <Feira/>
                     </Route>
+                </CarrinhoProvider>
+            </UsuarioProvider>
+            <Route path='/carrinho'>
+                <Carrinho/>
+            </Route>
             </Switch>
         </BrowserRouter>
     )
